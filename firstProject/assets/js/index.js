f192b6e1bf64fcf90916a9d4c6f5009e14ee0ba2
azerahmedov <ehmedovh111@gmail.com>
2021-11-07 20:21:54 +0400

    Ana sehife bitdi

--- firstProject/assets/js/index.js
+++ firstProject/assets/js/index.js
@@ -1,6 +1,17 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".Myswip", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
     },
   });

b356c4a5a61106c0d62c37ca844ee2fb90b8a736
azerahmedov <ehmedovh111@gmail.com>
2021-11-08 14:17:24 +0400

    ana sehifenin mobil versiyasi hazirlandi

--- firstProject/assets/js/index.js
+++ firstProject/assets/js/index.js
@@ -14,4 +14,37 @@ var swiper = new Swiper(".Myswip", {
     pagination: {
       el: ".swiper-pagination",
     },
+     breakpoints: {
+        // when window width is <= 499px
+        299: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 30
+        },
+        // when window width is <= 999px
+        999: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 40
+        },
+
+        1024: {
+          slidesPerView: 3,
+          spaceBetweenSlides: 40
+      },
+      2024: {
+        slidesPerView: 4,
+        spaceBetweenSlides: 40
+    },
+    },
   });
+
+  let mobilBar = document.querySelector('.mobil-bar')
+  let openBtn = document.querySelector('.openBtn')
+  let closeBtn = document.querySelector('.closeBtn');
+
+openBtn.addEventListener('click', () => {
+  mobilBar.classList.add('active') 
+})
+
+closeBtn.addEventListener('click', () => {
+  mobilBar.classList.remove('active') 
+})
